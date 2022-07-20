e259fb860717470af3ed0305892e44c43ba3906e
Hxnrique <67174506+Hxnrique@users.noreply.github.com>
2022-07-20 14:34:49 -0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,6 +2,6 @@ export default {
     discordAPIVersion: "https://discord.com/api/v10",
     discordCLIENTId: "691645044016545882",
     discordUSERAgent: "Octavia 0.0.1",
-    discordToken: "Bot NjkxNjQ1MDQ0MDE2NTQ1ODgy.Gfxu2B.6Q9LJylK5Lo6lzrK5WV7XM_nxs_LUybPad8KSE",
+    discordToken: "NjkxNjQ1MDQ0MDE2NTQ1ODgy.Gfxu2B.6Q9LJylK5Lo6lzrK5WV7XM_nxs_LUybPad8KSE",
     discordPUBLICKey: "e1dd1cb43746b20e120157c28b6f8bd7f658511c40e2150cdd6a461cc2360e23"
 } 

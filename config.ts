9023e865c949f7534c5f119911c96a08953f95e7
Hxnrique <67174506+Hxnrique@users.noreply.github.com>
2022-07-22 17:45:59 -0300

    Delete config.ts

--- config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    discordAPIVersion: "https://discord.com/api/v10",
-    discordCLIENTId: "691645044016545882",
-    discordUSERAgent: "Octavia 0.0.1",
-    discordToken: "NjkxNjQ1MDQ0MDE2NTQ1ODgy.GR6OAq.c0NblU5X91LYBg0wxQhkQ87Zu8xkw6pw_I9NY8",
-    discordPUBLICKey: "e1dd1cb43746b20e120157c28b6f8bd7f658511c40e2150cdd6a461cc2360e23"
-} 
